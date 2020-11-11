2656b4581f0008379d90ebc8894d308eb04dcf71
WimObiwan <wim@obiwan.be>
2020-11-11 11:52:35 +0100

    Fixed version hash

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace MathBlazor
                     InitialData = new Dictionary<string, string>() {
                         { "Version", 
                             $"{ThisAssembly.Git.SemVer.Major}.{ThisAssembly.Git.SemVer.Minor}.{ThisAssembly.Git.Commits}"
-                            + $"-{ThisAssembly.Git.Branch}+${ThisAssembly.Git.Commit}" }
+                            + $"-{ThisAssembly.Git.Branch}+{ThisAssembly.Git.Commit}" }
                     }
                 });
 
